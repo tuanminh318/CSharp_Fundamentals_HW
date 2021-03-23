d230509380d8255ff37c4df132cf931c93834861
tuanminh <75253755+projecttm318@users.noreply.github.com>
2021-03-24 00:17:31 +0700

    FindPrimeNumber

---
+++ Findprime/Program.cs
@@ -0,0 +1,36 @@
+﻿using Internal;
+using System;
+
+namespace Findprime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static async Task Main(string[] args)
+            {
+                Task t1 = TaskOne();
+                await t1;
+
+            }
+            public static bool CheckPrimeNumber(int n)
+            {
+                for (int i = 2; i <= Math.Sqrt(n); i++)
+                    if (n % i == 0)
+                        return false;
+                return true;
+            }
+            public static async Task TaskOne()
+            {
+                await ShowPrimeNumber.Delay(1000);
+                for (i = 1; i<=100; i++)
+                {
+                    if (CheckPrimeNumber(i))
+                    {
+                        Console.Write("{0, 5}",i);
+                    }
+                }
+            }
+        }
+    }
+}
